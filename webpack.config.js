387f75398ffe67cb55715b100ccba93089556581
Agah <agahkarakuzu@gmail.com>
2025-04-28 11:44:58 -0400

    tsxify

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const webpack = require("webpack");
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: "development",
+  context: path.resolve(__dirname, "static"),
+  entry: "./js/index.jsx",
+  output: {
+    path: path.resolve(__dirname, "dist/"),
+    filename: "bundle.js",
+    publicPath: "auto",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+  ],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(t|j)sx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "ts-loader",
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // Set publicPath as relative path ("./").
+              // By default it uses the `output.publicPath` ("/static/dist/"), when it rewrites the URLs in styles.css.
+              // And it causes these files unavailabe if BinderHub has a different base_url than "/".
+              publicPath: "./",
+            },
+          },
+          "css-loader",
+        ],
+      },
+      {
+        test: /\.(scss)$/,
+        use: [
+          {
+            // Adds CSS to the DOM by injecting a `<style>` tag
+            loader: "style-loader",
+          },
+          {
+            // Interprets `@import` and `url()` like `import/require()` and will resolve them
+            loader: "css-loader",
+          },
+          {
+            // Loader for webpack to process CSS with PostCSS
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [autoprefixer],
+              },
+            },
+          },
+          {
+            // Loads a SASS/SCSS file and compiles it to CSS
+            loader: "sass-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(eot|woff|ttf|woff2|svg|ico)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devtool: "source-map",
+};
